e6c3678d7b165c3a38c97358b40ff575a5185fe6
gustavols12 <gustavols2100@gmail.com>
2025-07-30 17:58:09 -0300

    Feat: cria componente de carrinho

--- src/app/vendas/components/index.tsx
+++ src/app/vendas/components/index.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { FiShoppingCart } from 'react-icons/fi';
+import { FiShoppingCart, FiTrash2 } from 'react-icons/fi';
 import { FormEvent, useState, useEffect } from 'react';
 import { ProductsPros } from '@/app/produtos/@types';
 import { MdRestartAlt } from 'react-icons/md';
@@ -9,15 +9,29 @@ interface itemCartProps {
 }
 
 export function Cart({ cartItem }: itemCartProps) {
-  const [cart, setCart] = useState(0);
   const [selectedProductId, setSelectedProductId] = useState('');
   const [unitPrice, setUnitPrice] = useState(0);
   const [qtd, setQtd] = useState(1);
   const [total, setTotal] = useState(0);
+  const [cart, setCart] = useState<ProductsPros[]>([]);
+
+  const totalCarrinho = cart.reduce((acc, item) => {
+    const quantidade = parseFloat(item.un);
+    const precoUnitario = parseFloat(item.price);
+    return acc + quantidade * precoUnitario;
+  }, 0);
 
   function handleAddItemCart(e: FormEvent) {
     e.preventDefault();
-    setCart(cart + 1);
+    setCart((prevCart) => [
+      ...prevCart,
+      {
+        name: selectedProductId,
+        price: total.toString(),
+        un: qtd.toString(),
+        id: selectedProductId,
+      },
+    ]);
   }
 
   useEffect(() => {
@@ -40,7 +54,7 @@ export function Cart({ cartItem }: itemCartProps) {
   return (
     <section className="grid grid-cols-1 lg:grid-cols-2">
       {/* adicionar  */}
-      <div className="w-full sm:px-8 flex flex-col items-center sm:items-start mb-8">
+      <div className="w-full sm:px-8 flex-1 flex-col items-center sm:items-start mb-8">
         <form
           onSubmit={handleAddItemCart}
           id="FormVenda"
@@ -133,80 +147,44 @@ export function Cart({ cartItem }: itemCartProps) {
         </form>
       </div>
       {/* finalizar */}
-      <div className="w-full sm:px-8 flex flex-col items-center sm:items-start">
-        {cart > 0 ? (
-          <form
-            action=""
-            id="FormVenda"
-            className=" flex flex-col rounded-xl gap-4 bg-white p-4 shadow-md max-w-2xl"
-          >
-            <div className="flex gap-2">
+      <div className="w-full sm:px-8 flex-1 flex-col items-center sm:items-start">
+        {cart.length > 0 ? (
+          <section className=" w-full flex flex-col rounded-xl gap-4 bg-white p-4 shadow-md max-w-2xl">
+            <div className="flex gap-2 pb-8">
               <FiShoppingCart size={24} className="text-gray-800" />
               <h2 className="text-gray-800 mb-2 font-semibold">
-                Carrinho ({cart} itens)
+                Carrinho ({cart.length} itens)
               </h2>
             </div>
-            <label
-              htmlFor="produto"
-              className="text-sm font-medium text-gray-700"
-            >
-              Produto*
-            </label>
-            <select
-              name="produto"
-              id="produto"
-              className="w-full rounded-lg border border-gray-300 p-2 outline-blue-500"
-              required={true}
-            >
-              <option value="">TESTE</option>
-              <option value="">banana</option>
-              <option value="">sdasas</option>
-              <option value="">TESTE</option>
-            </select>
-            <div className=" flex flex-col sm:flex-row gap-2">
-              <div className="flex-1">
-                <label
-                  htmlFor="qtde"
-                  className="text-sm font-medium text-gray-700"
-                >
-                  qtde*
-                </label>
-                <input
-                  type="number"
-                  placeholder="quantidade"
-                  id="qtde"
-                  className="w-full rounded-lg border border-gray-300 p-2 outline-blue-500"
-                  required={true}
-                />
+            {cart.map((item) => (
+              <div
+                key={item.id}
+                className="w-full rounded-lg bg-slate-100 flex items-center justify-between px-4 py-2"
+              >
+                <div className="flex text-start flex-col">
+                  <p>
+                    {cartItem.find((p) => p.id === item.id)?.name ||
+                      'Produto desconhecido'}
+                  </p>
+
+                  <span className="text-gray-800 font-semibold">
+                    R$ {item.price}
+                  </span>
+                  <span className="text-gray-800 font-semibold">
+                    Qtde: {item.un}
+                  </span>
+                </div>
+                <FiTrash2 size={28} color="red" />
               </div>
-              <div className=" flex-1">
-                <label
-                  htmlFor="valor"
-                  className="text-sm font-medium text-gray-700"
-                >
-                  Valor unitário*
-                </label>
-                <input
-                  type="number"
-                  placeholder="0.00"
-                  id="valor"
-                  className="w-full rounded-lg border border-gray-300 p-2 outline-blue-500"
-                />
-              </div>
+            ))}
+            <div className="flex items-center justify-between px-4 py-2">
+              <span className="text-gray-800 font-semibold">
+                Total da venda:
+              </span>
+              <strong className="text-green-500">
+                R$ {totalCarrinho.toFixed(2)}
+              </strong>
             </div>
-            <label
-              htmlFor="total"
-              className="text-sm font-medium text-gray-700"
-            >
-              Valor total*
-            </label>
-            <input
-              type="number"
-              placeholder="0.00"
-              id="total"
-              className="w-full rounded-lg border border-gray-300 p-2 outline-blue-500"
-              required={true}
-            />
             <button
               type="submit"
               className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 rounded-lg transition duration-300 flex items-center justify-center gap-2"
@@ -214,7 +192,7 @@ export function Cart({ cartItem }: itemCartProps) {
               <FiShoppingCart size={24} color="white" />
               Finalizar carrinho
             </button>
-          </form>
+          </section>
         ) : (
           <div className="flex gap-2 pb-8">
             <FiShoppingCart size={24} className="text-gray-800" />
