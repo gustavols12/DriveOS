3ebf7359825f7c3ec0c36e66dc739d493f8b727f
gustavols12 <gustavols2100@gmail.com>
2025-08-15 13:35:57 -0300

    update:  passa as validações do cadastro de cliente para o zod

--- src/app/cliente/components/addClient/index.tsx
+++ src/app/cliente/components/addClient/index.tsx
@@ -1,39 +1,55 @@
 'use client';
-
-import { Input } from '@/components/input';
-import { FormEvent, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
 import toast from 'react-hot-toast';
 
 export function FormClient() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
+  const schema = z.object({
+    name: z.string().min(1, 'o nome é obrigatório'),
+    email: z.email('Digite um email válido').min(1, 'o email é obrigatório'),
+    phone: z.string().refine(
+      (value) => {
+        return /^(?:\(\d{2}\)\s?)?\d{11}$/.test(value);
+      },
+      {
+        message: 'o numero de telefone deve estar no formato (41)995566353',
+      },
+    ),
+  });
 
+  type FormData = z.infer<typeof schema>;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
   const router = useRouter();
 
-  async function handleAddClient(e: FormEvent) {
-    e.preventDefault();
-
-    if (!name || !phone || !email) {
-      toast.error('preencha os dados do cliente');
-      return;
-    }
-
+  async function handleAddClient(data: FormData) {
     try {
       const res = await fetch('/api/cliente', {
         method: 'POST',
-        body: JSON.stringify({ name, email, phone }),
+        body: JSON.stringify({
+          name: data.name,
+          email: data.email,
+          phone: data.phone,
+        }),
       });
 
       if (!res.ok) {
-        toast.error('Erro ao cadastrar cliente');
+        const { error } = await res.json();
+        toast.error(error);
         return;
       }
       toast.success('Cadastrado com sucesso');
-      setName('');
-      setPhone('');
-      setEmail('');
+      reset();
       router.refresh();
     } catch (error) {
       toast.error('Erro ao cadastrar cliente');
@@ -41,7 +57,7 @@ export function FormClient() {
   }
   return (
     <form
-      onSubmit={handleAddClient}
+      onSubmit={handleSubmit(handleAddClient)}
       className="w-full max-w-7xl mx-auto shadow shadow-gray-300 rounded-xl p-6 mb-4"
     >
       <h2 className="text-2xl font-bold text-gray-800 mb-6">
@@ -53,42 +69,48 @@ export function FormClient() {
           <label className="text-gray-700 font-semibold mb-1" htmlFor="name">
             Nome*
           </label>
-          <Input
+          <input
             id="name"
-            required
             type="text"
             placeholder="Nome do Cliente..."
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            className="w-full rounded-md h-10 text-black outline-none border border-gray-300 px-3 py-2 box-border"
+            {...register('name')}
           />
+          {errors.name && (
+            <p className="text-red-500 mt-1">{errors.name.message}</p>
+          )}
         </div>
 
         <div className="flex flex-col">
           <label htmlFor="email" className="text-gray-700 font-semibold mb-1">
             Email*
           </label>
-          <Input
+          <input
             id="email"
-            required
             type="email"
             placeholder="seuEmail@teste.com"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            className="w-full rounded-md h-10 text-black outline-none border border-gray-300 px-3 py-2 box-border"
+            {...register('email')}
           />
+          {errors.email && (
+            <p className="text-red-500 mt-1">{errors.email.message}</p>
+          )}
         </div>
 
         <div className="flex flex-col">
           <label htmlFor="phone" className="text-gray-700 font-semibold mb-1">
             telefone*
           </label>
-          <Input
+          <input
             id="phone"
-            required
             type="text"
             placeholder="41 9999-9999"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
+            className="w-full rounded-md h-10 text-black outline-none border border-gray-300 px-3 py-2 box-border"
+            {...register('phone')}
           />
+          {errors.phone && (
+            <p className="text-red-500 mt-1">{errors.phone.message}</p>
+          )}
         </div>
       </div>
 
